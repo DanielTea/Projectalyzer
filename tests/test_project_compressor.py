0da57ea01ca888bda404bcf1197d7bab29f2f82b
Daniel T <info@danieltremer.com>
2023-04-18 12:30:55 +0200

    testing

--- tests/test_project_compressor.py
+++ tests/test_project_compressor.py
@@ -3,6 +3,18 @@ from unittest.mock import patch
 from project_compression.project_compression import ProjectCompressor
 import os
 
+# Get the absolute path of the current file
+current_file_path = os.path.abspath(__file__)
+
+# Get the path of the "tests" folder
+tests_folder_path = os.path.dirname(current_file_path)
+
+# Get the path of the "test_data" folder
+test_data_folder_path = os.path.join(tests_folder_path, 'test_data')
+
+# Get the path of the "test_project" folder
+test_project_folder_path = os.path.join(test_data_folder_path, 'test_project')
+
 class TestProjectCompressor(unittest.TestCase):
 
     @patch("project_compression.project_compression.compress_string")
--- tests/test_token_estimator.py
+++ tests/test_token_estimator.py
@@ -1,5 +1,18 @@
 import unittest
 from project_compression.token_estimator import estimate_tokens
+import os
+
+# Get the absolute path of the current file
+current_file_path = os.path.abspath(__file__)
+
+# Get the path of the "tests" folder
+tests_folder_path = os.path.dirname(current_file_path)
+
+# Get the path of the "test_data" folder
+test_data_folder_path = os.path.join(tests_folder_path, 'test_data')
+
+# Get the path of the "test_project" folder
+test_project_folder_path = os.path.join(test_data_folder_path, 'test_project')
 
 class TestTokenEstimator(unittest.TestCase):
 
